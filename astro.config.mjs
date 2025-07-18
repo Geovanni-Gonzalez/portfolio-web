c3cd45d90f4c5deac22ca61a3048f0437559134d
Geovanni Gonzalez <geovanniga32@gmail.com>
2025-07-17 22:22:18 -0600

    Primera version

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,30 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig, fontProviders } from 'astro/config';
+import react from '@astrojs/react';
+import tailwindcss from '@tailwindcss/vite';
+
+import vercel from '@astrojs/vercel';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  integrations: [react()],
+
+  vite: {
+    plugins: [tailwindcss()]
+  },
+  experimental: {
+  fonts: [{
+    provider: fontProviders.google(),
+    name: "Geist",
+    cssVariable: "--font-geist",
+    fallbacks: ["Inter", "sans-serif"],
+  }]
+  },
+
+  i18n: {
+    defaultLocale: 'es',
+    locales: ['es', 'en']
+  },
+
+  adapter: vercel()
+});
---
+++ src/i18n/ui.ts
@@ -0,0 +1,42 @@
+export const languages = {
+  en: 'English',
+  es: 'Español',
+};
+
+export const defaultLang = 'es';
+
+export const ui = {
+    en: {
+        'app.title': 'Portfolio',
+        'app.description': 'A portfolio showcasing my work and skills.',
+        'nav.home': 'Home',
+        'nav.about': 'About',
+        'nav.projects': 'Projects',
+        'nav.contact': 'Contact',
+        'footer.copyright': 'Geovanni Portafolio, All Rights Reserved',
+        'footer.about': 'About',
+        'footer.projects': 'Projects',
+        'footer.contact': 'Contact',
+        'welcome.message': 'Welcome to my portfolio!',
+        'hero.greeting': 'Hello, I am Geovanni',
+        'hero.description': 'A passionate web developer focused on creating dynamic and responsive web applications.',
+        'hero.cta': 'Let\'s work together to build something amazing!',
+    },
+    
+    es: {
+        'app.title': 'Portafolio',
+        'app.description': 'Un portafolio que muestra mi trabajo y habilidades.',
+        'nav.home': 'Inicio',
+        'nav.about': 'Acerca de',
+        'nav.projects': 'Proyectos',
+        'nav.contact': 'Contacto',
+        'footer.copyright': 'Portafolio de Geovanni, Todos los Derechos Reservados',
+        'footer.about': 'Acerca de',
+        'footer.projects': 'Proyectos',
+        'footer.contact': 'Contacto',
+        'welcome.message': '¡Bienvenido a mi portafolio!',
+        'hero.greeting': 'Hola, soy Geovanni',
+        'hero.description': 'Un desarrollador web apasionado enfocado en crear aplicaciones web dinámicas y responsivas.',
+        'hero.cta': '¡Trabajemos juntos para construir algo increíble!'
+    },
+} as const;
