726a35a1dc9d59bbac86c48419ed3d9978db2286
Geovanni Gonzalez <geovanniga32@gmail.com>
2025-07-21 20:41:32 -0600

    Seccion About

--- src/i18n/ui.ts
+++ src/i18n/ui.ts
@@ -9,34 +9,42 @@ export const ui = {
     en: {
         'app.title': 'Portfolio',
         'app.description': 'A portfolio showcasing my work and skills.',
+
         'nav.home': 'Home',
-        'nav.about': 'About',
+        'nav.aboutMe': 'About Me',
         'nav.projects': 'Projects',
         'nav.contact': 'Contact',
+
         'footer.copyright': 'Geovanni Portafolio, All Rights Reserved',
-        'footer.about': 'About',
+        'footer.aboutMe': 'About Me',
         'footer.projects': 'Projects',
         'footer.contact': 'Contact',
-        'welcome.message': 'Welcome to my portfolio!',
-        'hero.greeting': 'Hello, I am Geovanni',
-        'hero.description': 'A passionate web developer focused on creating dynamic and responsive web applications.',
+
+
+        'hero.name': 'Geovanni',
+        'hero.lastname': 'González',
+        'hero.title': 'Computer Engineering Student',
         'hero.cta': 'Let\'s work together to build something amazing!',
     },
     
     es: {
         'app.title': 'Portafolio',
         'app.description': 'Un portafolio que muestra mi trabajo y habilidades.',
+
         'nav.home': 'Inicio',
-        'nav.about': 'Acerca de',
+        'nav.aboutMe': 'Sobre mí',
         'nav.projects': 'Proyectos',
         'nav.contact': 'Contacto',
+
         'footer.copyright': 'Portafolio de Geovanni, Todos los Derechos Reservados',
-        'footer.about': 'Acerca de',
+        'footer.aboutMe': 'Sobre mí',
         'footer.projects': 'Proyectos',
         'footer.contact': 'Contacto',
-        'welcome.message': '¡Bienvenido a mi portafolio!',
-        'hero.greeting': 'Hola, soy Geovanni',
-        'hero.description': 'Un desarrollador web apasionado enfocado en crear aplicaciones web dinámicas y responsivas.',
-        'hero.cta': '¡Trabajemos juntos para construir algo increíble!'
+
+        'hero.name': 'Geovanni',
+        'hero.lastname': 'González',
+        'hero.title': 'Estudiante de Ingeniería en Computación',
+        'hero.cta': '¡Trabajemos juntos para construir algo increíble!',
+        'hero.subtitle': 'Desarrollador Frontend y Backend, apasionado por la tecnología y el aprendizaje continuo.',
     },
 } as const;
